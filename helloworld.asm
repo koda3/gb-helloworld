0b02d8a5ec8da3b320958b703e42754b9e52ed5b
kodabe <7932murasaki@gmail.com>
2018-04-25 01:50:01 -0400

    「あいうえお」を表示

--- helloworld.asm
+++ helloworld.asm
@@ -1,88 +1,88 @@
 SECTION "start",ROM0[$0100]
 	nop
-	jp 		$0150 ; 開始位置
+	jp		$0150 ; 開始位置
 
 	; $0150までカートリッジヘッダ
 	; 任天堂ロゴ
-	db 		$CE, $ED, $66, $66, $CC, $0D, $00, $0B, $03, $73, $00, $83, $00, $0C, $00, $0D
-	db 		$00, $08, $11, $1F, $88, $89, $00, $0E, $DC, $CC, $6E, $E6, $DD, $DD, $D9, $99
-	db 		$BB, $BB, $67, $63, $6E, $0E, $EC, $CC, $DD, $DC, $99, $9F, $BB, $B9, $33, $3E
-	db 		"HELLOWORLD", $00, $00, $00, $00, $00
-	db 		$00, $00, $00, $00, $00, $00, $00, $01, $33
-	db 		$00, $00, $00
+	db		$CE, $ED, $66, $66, $CC, $0D, $00, $0B, $03, $73, $00, $83, $00, $0C, $00, $0D
+	db		$00, $08, $11, $1F, $88, $89, $00, $0E, $DC, $CC, $6E, $E6, $DD, $DD, $D9, $99
+	db		$BB, $BB, $67, $63, $6E, $0E, $EC, $CC, $DD, $DC, $99, $9F, $BB, $B9, $33, $3E
+	db		"HELLOWORLD", $00, $00, $00, $00, $00
+	db		$00, $00, $00, $00, $00, $00, $00, $01, $33
+	db		$00, $00, $00
 
 SECTION "ROM0", ROM0[$150]
 main:
-	ld 		a, %11100100
-	ld 		[$FF47], a
+	ld		a, %11100100
+	ld		[$FF47], a
 
-	ld 		a, $00
-	ld 		[$FF42], a ; スクロールX座標
-	ld 		[$FF43], a ; スクロールY座標
+	ld		a, $00
+	ld		[$FF42], a ; スクロールX座標
+	ld		[$FF43], a ; スクロールY座標
 
-	call 	load_tiles
-	call 	clear_lcd
-	ld 		bc, 0
+	call	load_tiles
+	call	clear_lcd
+	ld		bc, 0
 .loop_1:
-	inc 	bc
-	ld 		hl, $9800
-	add 	hl, bc
-	ld 		[hl], c
+	inc		bc
+	ld		hl, $9800
+	add		hl, bc
+	ld		[hl], c
 
-	ld 		a, c
-	sub 	5
-	jp 		nz, .loop_1
+	ld		a, c
+	sub		5
+	jp		nz, .loop_1
 
-	ld 		a, %10010001
-	ld 		[$FF40], a
+	ld		a, %10010001
+	ld		[$FF40], a
 
 ; 無限ループ
 loop:
 	halt
 	nop
-	jr 		loop
+	jr		loop
 
 load_tiles:
-	call 	lcd_off
-	ld 		hl, $4000
-	ld 		de, $8000
-	ld 		b, 16*6
+	call	lcd_off
+	ld		hl, $4000
+	ld		de, $8000
+	ld		b, 16*6
 .loop:
-	ld 		a, [hl]
-	ld 		[de], a
-	dec 	b
-	ret 	z
-	inc 	hl
-	inc 	de
-	jr 		.loop
+	ld		a, [hl]
+	ld		[de], a
+	dec		b
+	ret		z
+	inc		hl
+	inc		de
+	jr		.loop
 
 clear_lcd:
-	ld 		de, 32*32
+	ld		de, 32*32
 .loop:
-	ld 		a, 0
-	ld 		[hl], a
-	dec 	de
-	ld 		a, d
-	or 		e
+	ld		a, 0
+	ld		[hl], a
+	dec		de
+	ld		a, d
+	or		e
 	ret		z
-	inc 	hl
-	jp 		.loop
+	inc		hl
+	jp		.loop
 
 lcd_off:
 	; LCDがオフなら抜ける
-	ld 		a, [$FF44] ; LCDC Y座標
+	ld		a, [$FF44] ; LCDC Y座標
 	rlca
-	ret 	nc
+	ret		nc
 .wait_vblank
 	; LCDが書き込み終わるまでループします
-	ld 		a, [$FF44] ; LCDC Y座標
-	cp 		$91
-	jr 		nz, .wait_vblank
+	ld		a, [$FF44] ; LCDC Y座標
+	cp		$91
+	jr		nz, .wait_vblank
 
 	; LCDをオフにします
-	ld 		a, [$FF40] ; LCD制御
-	res 	$07, a     ; ７ビット目をリセット(０にされる)
-	ld 		[$FF40], a
+	ld		a, [$FF40] ; LCD制御
+	res		$07, a		 ; ７ビット目をリセット(０にされる)
+	ld		[$FF40], a
 	ret
 
 SECTION "Tiles", ROMX ; $8000
