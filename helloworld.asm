9251e30616d3dcc238df4161049fcb68c0731c1c
kodabe <7932murasaki@gmail.com>
2018-04-26 20:49:13 -0400

    前より簡単に文字列を表示できます

--- helloworld.asm
+++ helloworld.asm
@@ -1,3 +1,8 @@
+mov: MACRO
+	ld	a, \2
+	ld	\1, a
+ENDM
+
 SECTION "start",ROM0[$0100]
 	nop
 	jp	$0150 ; 開始位置
@@ -22,19 +27,12 @@ main:
 
 	call	load_tiles
 	call	clear_lcd
-	ld	bc, 0
-.loop_1:
-	inc	bc
-	ld	hl, $9800
-	add	hl, bc
-	ld	[hl], c
-
-	ld	a, c
-	sub	5
-	jp	nz, .loop_1
-
-	ld	a, %10010001
-	ld	[$FF40], a
+	mov	[$C000], $06 ; こ
+	mov	[$C001], $03 ; う
+	mov	[$C002], $00 ; 　
+	mov	[$C003], $09 ; で
+	mov	[$C004], $0A ; す
+	call write_out
 
 ; 無限ループ
 loop:
@@ -42,21 +40,27 @@ loop:
 	nop
 	jr	loop
 
+write_out: 
+	; $C000にある文字列を表示する
+	call 	lcd_off
+	ld	bc, 32*32
+	ld	de, $C000
+	ld	hl, $9800
+	call 	memcpy
+	ld	a, %10010001
+	ld	[$FF40], a
+	ret
+
 load_tiles:
 	call	lcd_off
-	ld	hl, $4000
-	ld	de, $8000
-	ld	b, 16*6
-.loop:
-	ld	a, [hl]
-	ld	[de], a
-	dec	b
-	ret	z
-	inc	hl
-	inc	de
-	jr	.loop
+	ld	bc, 16*11
+	ld	de, $4000
+	ld	hl, $8000
+	call	memcpy
+	ret
 
 clear_lcd:
+	ld 	hl, $9800
 	ld	de, 32*32
 .loop:
 	ld	a, 0
@@ -70,25 +74,45 @@ clear_lcd:
 
 lcd_off:
 	; LCDがオフなら抜ける
-	ld	a, [$FF44] ; LCDC Y座標
+	ld	a, [$FF44]	; LCDC Y座標
 	rlca
 	ret	nc
 .wait_vblank
 	; LCDが書き込み終わるまでループします
-	ld	a, [$FF44] ; LCDC Y座標
+	ld	a, [$FF44]	; LCDC Y座標
 	cp	$91
 	jr	nz, .wait_vblank
 
 	; LCDをオフにします
-	ld	a, [$FF40] ; LCD制御
-	res	$07, a		 ; ７ビット目をリセット(０にされる)
+	ld	a, [$FF40]	; LCD制御
+	res	$07, a		; ７ビット目をリセット(０にされる)
 	ld	[$FF40], a
 	ret
 
-SECTION "Tiles", ROMX ; $8000
+memcpy: 
+	; bc コピーサイズ
+	; de コピー元のメモリポインター
+	; hl コピー先のメモリポインター
+.loop:
+	ld	a, [de]
+	ld	[hl], a
+	inc	de
+	inc 	hl
+	dec	bc
+	ld	a, b
+	or	c
+	jp	nz, .loop
+	ret
+
+SECTION "Tiles", ROMX ; $4000
 db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00 ; 空
 db $00, $00, $20, $20, $7C, $7C, $20, $20, $3C, $3C, $6A, $6A, $B2, $B2, $64, $64 ; あ
 db $00, $00, $00, $00, $88, $88, $84, $84, $82, $82, $82, $82, $50, $50, $20, $20 ; い
 db $00, $00, $3C, $3C, $00, $00, $3C, $3C, $42, $42, $02, $02, $04, $04, $38, $38 ; う
 db $00, $00, $3C, $3C, $00, $00, $7C, $7C, $08, $08, $18, $18, $28, $28, $46, $46 ; え
 db $00, $00, $20, $20, $F4, $F4, $22, $22, $3C, $3C, $62, $62, $A2, $A2, $6C, $6C ; お
+db $00, $00, $00, $00, $3C, $3C, $00, $00, $00, $00, $20, $20, $40, $40, $3E, $3E ; こ
+db $00, $00, $2A, $2A, $F0, $F0, $2E, $2E, $40, $40, $48, $48, $50, $50, $8E, $8E ; だ
+db $00, $00, $0A, $0A, $20, $20, $50, $50, $88, $88, $06, $06, $00, $00, $00, $00 ; べ
+db $00, $00, $0E, $0E, $74, $74, $0A, $0A, $10, $10, $10, $10, $08, $08, $06, $06 ; で
+db $00, $00, $08, $08, $FE, $FE, $18, $18, $28, $28, $18, $18, $08, $08, $10, $10 ; す
